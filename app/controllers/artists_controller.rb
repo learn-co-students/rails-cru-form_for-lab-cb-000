5bd43af8c16b23192c261cd5658667398355fe17
dustinlikens <dustinlikens@icloud.com>
2017-02-23 02:48:08 +0100

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,43 @@
+class ArtistsController < ApplicationController
+  # include Memorable::InstanceMethods
+
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.create(white_params)
+    redirect_to artist_path(@artist)
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(white_params)
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  def delete
+    @artist = Artist.find(params[:id])
+    @artist.delete
+    redirect_to artists_path
+  end
+
+  private
+
+  def white_params
+    params.require(:artist).permit(:name, :bio)
+  end
+end
