9ec96f19426ed808c629c7a03b1df9826dcd617b
sjmoody <steven@beachheadmarketing.com>
2017-11-09 14:28:57 +0100

    object relationships defined in models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :songs
+  resources :songs
   resources :artists
   resources :genres
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109131329) do
+ActiveRecord::Schema.define(version: 20171109132122) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -29,8 +29,7 @@ ActiveRecord::Schema.define(version: 20171109131329) do
   create_table "songs", force: :cascade do |t|
     t.string   "name"
     t.integer  "artist_id"
-    t.string   "genre_id"
-    t.string   "integer"
+    t.integer  "genre_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
