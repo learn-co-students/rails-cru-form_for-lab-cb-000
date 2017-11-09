7dfeadedf9e5992898f402ff87d36a98dfa276c3
sjmoody <steven@beachheadmarketing.com>
2017-11-09 15:15:47 +0100

    passing genre spec

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,10 +1,36 @@
 class GenresController < ApplicationController
+
+  def index
+    @genres = Genre.all
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
   def create
+    @genre = Genre.new(genre_params(:name))
+    @genre.save
+    redirect_to genre_path(@genre)
   end
 
   def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
-  end 
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params(:name))
+    redirect_to genre_path(@genre)
+  end
+
+private
+
+  def genre_params(*args)
+    params.require(:genre).permit(*args)
+  end
 end
