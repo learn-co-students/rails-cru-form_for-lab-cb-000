55daf751349287ce60ce7da67917cac380d27f23
Sergio Marrocoli <s.marrocoli@gmail.com>
2019-02-26 19:46:02 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,18 +1,25 @@
 class SongsController < ApplicationController
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def create
+    @song = song.create(params.require(:song).permit(:name, :genre_id, :artist_id))
+    redirect_to song_path(@song)
   end
 
   def new
   end
 
   def edit
+      @song = song.find(params[:id])
   end
 
   def update
+    @song = song.find(params[:id])
+    @song.update(params.require(:song).permit(:name, :genre_id, :artist_id))
+    redirect_to song_path(@song)
   end
 
 end
